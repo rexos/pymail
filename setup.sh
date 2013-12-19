b2775df9b8c661b9815a66dff2aa679669c03082
rexos <alex.pellegrini@live.com>
2013-12-19 13:36:06 +0100

    adds external profile file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
 #! /bin/bash
 
+PYMAIL="$HOME/.pymail_profile.sh"
+profile="$HOME/.bash_profile"
+
+function update_bash(){
+    echo "# pymail setup" >> $profile
+    echo "if [ -f ~/.pymail_profile.sh ]; then" >> $profile
+    echo "    source ~/.pymail_profile.sh" >> $profile
+    echo "fi" >> $profile
+}
+
+touch $PYMAIL
 echo "mail addr : "
 read acc
-echo "export PYMAIL_ADR=$acc" >> ~/.bashrc
+echo "#! /bin/bash" > $PYMAIL
+echo "export PYMAIL_ADR=$acc" >> $PYMAIL
 echo "mail pass :"
 read psw
-echo "export PYMAIL_PSW=$(md5 -q -s $psw)" >> ~/.bashrc
+echo "export PYMAIL_PSW=$(md5 -q -s $psw)" >> $PYMAIL
+sudo chmod +x $PYMAIL
+update_bash
 echo "Done, restart the shell to make changes effective"
